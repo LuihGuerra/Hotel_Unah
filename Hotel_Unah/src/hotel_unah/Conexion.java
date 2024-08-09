bd5b42504e4a587bfb40e780e131735c9484632b
LuihGuerra <zavalafernando262@gmail.com>
2024-08-08 21:52:14 -0600

    Add files via upload

---
+++ Hotel_Unah/src/hotel_unah/Conexion.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hotel_unah;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Luih
+ */
+public class Conexion {
+    
+
+    
+    private Conexion(){}
+    //establecer credenciales
+    
+    private static Connection conn;
+    
+    private static Conexion instancia;
+    
+    private static final String url = "jdbc:mysql://localhost/hotel_unah";
+    
+    private static final String user = "root";
+    
+    private static final String password = "";
+    
+    //establecer conexion
+    
+    public Connection conectar(){
+        try{
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            conn=DriverManager.getConnection(url,user,password);
+            
+            //JOptionPane.showMessageDialog(null, "Conexion Exitosa !!");
+            
+            return conn;
+        
+        }catch(Exception e){
+        
+            JOptionPane.showMessageDialog(null,"Error: "+ e);
+        
+        }
+        
+        return conn;
+        
+    }
+    
+    //cerrar conexion
+    
+    public void cerrarConexion() throws SQLException{
+            try{
+            
+                   conn.close();
+                   //JOptionPane.showMessageDialog(null, "Conexion Cerrada");
+            }catch(Exception e){
+            
+                JOptionPane.showMessageDialog(null,"Error"+ e);
+                conn.close();
+            }finally{
+                conn.close();
+            }
+            
+            
+    }
+    
+    public static Conexion getInstance(){
+        
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+}
---
+++ Hotel_Unah/src/hotel_unah/Hotel_Unah.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hotel_unah;
+
+/**
+ *
+ * @author Luih
+ */
+public class Hotel_Unah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Main_Hotel ventana_principal = new Main_Hotel();
+        
+        ventana_principal.setVisible(true);
+    }
+    
+}
